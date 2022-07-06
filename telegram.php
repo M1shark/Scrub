c84ca5fc0a5e7b07cfd8798ad0ece3a3684181be
Михаил <88690428+M1shark@users.noreply.github.com>
2022-07-06 14:56:31 +0300

    Add files via upload

---
+++ telegram.php
@@ -0,0 +1,26 @@
+<?php
+
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$token = "5342674595:AAHYOMqx76uz6jUAFVNIhBWA1O10IlNCF7A";
+$chat_id = "-741120216";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+  if ($sendToTelegram) {
+    header('Location: thanks.html');
+  } else {
+    echo "Error";
+  }
+  ?>
+  
+?>
